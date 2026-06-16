b32b720fadfdf62d0dbf850ed37172fe148532f5
DFN <daviferreiraneris@gmail.com>
2026-06-16 20:05:06 -0300

    Cookies na pasta correta

--- at.php
+++ at.php
@@ -2,24 +2,24 @@
 
 if (isset($_GET['tema'])) {
     $opcao_tema = $_GET['tema'];
-    
-    
     setcookie("tema_usuario", $opcao_tema, time() + 3600, "/");
     
     
-    header("Location: index.php");
+    header("Location: /att/cookie/at.php");
+    exit;
+}
+
+
+if (isset($_GET['resetar'])) {
+    setcookie("tema_usuario", "", time() - 3600, "/");
+    
+   
+    header("Location: /att/cookie/at.php");
     exit;
 }
 
 
 $tema_atual = isset($_COOKIE['tema_usuario']) ? $_COOKIE['tema_usuario'] : 'claro';
-
-
-if (isset($_GET['resetar'])) {
-    setcookie("tema_usuario", "", time() - 3600, "/");
-    header("Location: index.php");
-    exit;
-}
 ?>
 
 <!DOCTYPE html>
@@ -31,7 +31,6 @@ if (isset($_GET['resetar'])) {
         body { 
             font-family: Arial, sans-serif; 
             padding: 40px; 
-            /* Muda a cor de fundo baseado no valor do cookie */
             background-color: <?php echo $tema_atual === 'escuro' ? '#333' : '#fff'; ?>;
             color: <?php echo $tema_atual === 'escuro' ? '#fff' : '#333'; ?>;
         }
@@ -41,15 +40,12 @@ if (isset($_GET['resetar'])) {
 <body>
 
     <h1>O tema atual é: <?php echo ucwords($tema_atual); ?></h1>
-
-    
     <p>
-        <a href="index.php?tema=claro">Ativar Tema Claro</a>
-        <a href="index.php?tema=escuro">Ativar Tema Escuro</a>
-        <a href="index.php?resetar=1">Deletar Cookie (Resetar)</a>
+        
+        <a href="/att/cookie/at.php?tema=claro">Ativar Tema Claro</a>
+        <a href="/att/cookie/at.php?tema=escuro">Ativar Tema Escuro</a>
+        <a href="/att/cookie/at.php?resetar=1">Deletar Cookie (Resetar)</a>
     </p>
-
-    <p>Dica: Modifique o tema e atualize a página manualmente para ver que a sua escolha permanece salva!</p>
 
 </body>
 </html>
